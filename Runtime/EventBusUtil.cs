2208ae2331e44c09ddc536be3874e5aaf07799f9
MariusGr <marius.gripp@arcor.de>
2024-09-11 10:13:41 +0200

    Register EventBindings by Event Type name

--- Runtime/EventBusUtil.cs
+++ Runtime/EventBusUtil.cs
@@ -9,31 +9,39 @@ using UnityEngine;
 using MyBox;
 #endif
 
-namespace UnityEventBus {
+namespace UnityEventBus
+{
     /// <summary>
     /// Contains methods and properties related to event buses and event types in the Unity application.
     /// </summary>
-    public static class EventBusUtil {
+    public static class EventBusUtil
+    {
         public static IReadOnlyList<Type> EventTypes { get; set; }
         public static IReadOnlyList<Type> EventBusTypes { get; set; }
 
         private static readonly Dictionary<string, Action> _eventRaisers = new();
+        private static readonly Dictionary<string, Action<Action>> _eventRegisterers = new();
 
-        private static IEnumerable<Type> GetEventTypes() {
+        private static IEnumerable<Type> GetEventTypes()
+        {
             return AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(s => s.GetTypes())
                 .Where(p => typeof(IEvent).IsAssignableFrom(p) && !p.IsInterface && !p.IsAbstract);
         }
 
         private static void Raise<T>() where T : IEvent, new() => EventBus<T>.Raise(new T());
+        private static void Register<T>(Action onEvent) where T : IEvent, new() => EventBus<T>.Register(new EventBinding<T>(onEvent));
 
         static EventBusUtil()
         {
             foreach (var type in GetEventTypes())
             {
-                var genericMethod = typeof(EventBusUtil).GetMethod(nameof(Raise), BindingFlags.Static | BindingFlags.NonPublic).MakeGenericMethod(type);
-                _eventRaisers.Add(type.Name, () => genericMethod.Invoke(null, new object[0]));
-            }
+                var genericRaise = typeof(EventBusUtil).GetMethod(nameof(Raise), BindingFlags.Static | BindingFlags.NonPublic).MakeGenericMethod(type);
+                _eventRaisers.Add(type.Name, () => genericRaise.Invoke(null, new object[0]));
+
+                var genericRegister = typeof(EventBusUtil).GetMethod(nameof(Register), BindingFlags.Static | BindingFlags.NonPublic).MakeGenericMethod(type);
+                _eventRegisterers.Add(type.Name, onEvent => genericRegister.Invoke(null, new object[] { onEvent }));
+            };
         }
 
         public static void RaiseEvent(string eventName)
@@ -43,10 +51,18 @@ namespace UnityEventBus {
             else
                 Debug.LogError($"Event {eventName} not found");
         }
-        
-    #if UNITY_EDITOR
+
+        public static void RegisterEvent(string eventName, Action onEvent)
+        {
+            if (_eventRegisterers.TryGetValue(eventName, out var registerEvent))
+                registerEvent(onEvent);
+            else
+                Debug.LogError($"Event {eventName} not found");
+        }
+
+#if UNITY_EDITOR
         public static PlayModeStateChange PlayModeState { get; set; }
-        
+
         /// <summary>
         /// Initializes the Unity Editor related components of the EventBusUtil.
         /// The [InitializeOnLoadMethod] attribute causes this method to be called every time a script
@@ -57,14 +73,17 @@ namespace UnityEventBus {
         /// actions to be performed when the Editor's play mode changes.
         /// </summary>    
         [InitializeOnLoadMethod]
-        public static void InitializeEditor() {
+        public static void InitializeEditor()
+        {
             EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
             EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
         }
-        
-        static void OnPlayModeStateChanged(PlayModeStateChange state) {
+
+        static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
             PlayModeState = state;
-            if (state == PlayModeStateChange.ExitingPlayMode) {
+            if (state == PlayModeStateChange.ExitingPlayMode)
+            {
                 ClearAllBuses();
             }
         }
@@ -73,11 +92,11 @@ namespace UnityEventBus {
         {
             var eventOptions = GetEventTypes().Select(type => new LabelValuePair(type.Name, type.Name)).ToArray();
             eventOptions = eventOptions.OrderBy(option => option.Label).ToArray();
-            var optionsWithNone = new List<LabelValuePair> {new LabelValuePair("None", "")};
+            var optionsWithNone = new List<LabelValuePair> { new LabelValuePair("None", "") };
             optionsWithNone.AddRange(eventOptions);
             return optionsWithNone.ToArray();
         }
-    #endif
+#endif
 
         /// <summary>
         /// Initializes the EventBusUtil class at runtime before the loading of any scene.
@@ -87,28 +106,33 @@ namespace UnityEventBus {
         /// done before any game objects, scripts or components have started.
         /// </summary>
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        public static void Initialize() {
+        public static void Initialize()
+        {
             EventTypes = PredefinedAssemblyUtil.GetTypes(typeof(IEvent));
             EventBusTypes = InitializeAllBuses();
         }
 
-        static List<Type> InitializeAllBuses() {
+        static List<Type> InitializeAllBuses()
+        {
             List<Type> eventBusTypes = new();
-            
+
             var typedef = typeof(EventBus<>);
-            foreach (var eventType in EventTypes) {
+            foreach (var eventType in EventTypes)
+            {
                 var busType = typedef.MakeGenericType(eventType);
                 eventBusTypes.Add(busType);
             }
-            
+
             return eventBusTypes;
         }
 
         /// <summary>
         /// Clears (removes all listeners from) all event buses in the application.
         /// </summary>
-        public static void ClearAllBuses() {
-            for (int i = 0; i < EventBusTypes.Count; i++) {
+        public static void ClearAllBuses()
+        {
+            for (int i = 0; i < EventBusTypes.Count; i++)
+            {
                 var busType = EventBusTypes[i];
                 var clearMethod = busType.GetMethod("Clear", BindingFlags.Static | BindingFlags.NonPublic);
                 clearMethod?.Invoke(null, null);
