d223784e98562babe3bdea256ba59b12e8b931be
Marius Gripp <marius.gripp@arcor.de>
2024-09-17 13:43:56 +0200

    Propagate eventObject parameter in Raise by type name

--- Runtime/Events.cs
+++ Runtime/Events.cs
@@ -1,7 +1,9 @@
 ﻿namespace UnityEventBus {
     public interface IEvent { }
 
-    public struct TestEvent : IEvent { }
+    public struct TestEvent : IEvent {
+        public int health;
+    }
 
     public struct PlayerEvent : IEvent {
         public int health;
