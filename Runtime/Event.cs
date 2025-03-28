cbf3f024347264a112aa5cd5c048d878860adf96
MariusGr <marius.gripp@arcor.de>
2025-03-28 14:54:56 +0100

    Implement Event base class

---
+++ Runtime/Event.cs
@@ -0,0 +1,7 @@
+﻿namespace UnityEventBus {
+    public abstract class Event : IEvent {
+        public override bool Equals(object obj) => obj != null && obj.GetType() == GetType();
+        public override int GetHashCode() => GetType().GetHashCode();
+        public override string ToString() => GetType().Name;
+    }
+}
