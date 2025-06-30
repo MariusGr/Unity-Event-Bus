9e214be6b0a1fd7849cb08c0f951e5af5854bd36
MariusGr <marius.gripp@arcor.de>
2025-06-30 17:45:41 +0200

    Optionally save sender to Events

--- Runtime/Event.cs
+++ Runtime/Event.cs
@@ -1,7 +1,17 @@
 ﻿namespace UnityEventBus {
-    public abstract class Event : IEvent {
+    public abstract class Event : IEvent
+    {
+        public object Sender { get; set; }
         public override bool Equals(object obj) => obj != null && obj.GetType() == GetType();
         public override int GetHashCode() => GetType().GetHashCode();
         public override string ToString() => GetType().Name;
+        public Event() { }
+        public Event(object sender) : this() => Sender = sender;
+        public bool IsSender(object sender)
+        {
+            if (sender == null) return false;
+            if (Sender == null) return false;
+            return Sender == sender;
+        }
     }
 }
